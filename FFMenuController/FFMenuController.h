94d0b90373371b2bfd62160dfb945f8f456a2b1f
Febin Fathah <febinfathah@gmail.com>
2018-11-29 16:52:35 +0530

    Initial Commit

---
+++ FFMenuController/FFMenuController.h
@@ -0,0 +1,19 @@
+//
+//  FFMenuController.h
+//  FFMenuController
+//
+//  Created by Febin Fathah on 29/11/18.
+//  Copyright © 2018 Febin Fathah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFMenuController.
+FOUNDATION_EXPORT double FFMenuControllerVersionNumber;
+
+//! Project version string for FFMenuController.
+FOUNDATION_EXPORT const unsigned char FFMenuControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFMenuController/PublicHeader.h>
+
+
---
+++ FFMenuControllerTests/FFMenuControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  FFMenuControllerTests.swift
+//  FFMenuControllerTests
+//
+//  Created by Febin Fathah on 29/11/18.
+//  Copyright © 2018 Febin Fathah. All rights reserved.
+//
+
+import XCTest
+@testable import FFMenuController
+
+class FFMenuControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
